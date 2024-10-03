59d6a212fa9a73943e537703392b9c96b26b4d7b
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-10-03 11:49:03 -0400

    bug fix

--- src/maths/Goldbach.java
+++ src/maths/Goldbach.java
@@ -1,11 +1,29 @@
 package maths;
 
 /**
- * A program that attempts to disprove Goldbach's Conjecture
- * @author Tarkan Zarrouk
- * @version 1.1 10/02/2024
+ * author: Tarkan Zarrouk
+ * date: 10/03/2024
  * Goldbach's conjecture program
  */
+
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
+ // comments
 
 import java.util.Scanner;
 
@@ -15,9 +33,10 @@ public class Goldbach {
 
         int value = getNumber(input);
         while (value != 0) {
-            value = getNumber(input);
             testGoldbach(value);
-            getPrimeAfter(value);
+            int nextPrime = getPrimeAfter(value);
+            System.out.println("Next prime after " + value + " is " + nextPrime);
+            value = getNumber(input);
         }
     }
 
@@ -30,8 +49,11 @@ public class Goldbach {
      * @return either 0 or an even number > 2
      */
     public static int getNumber(Scanner input) {
-        System.out.print("Enter an even non-negative integer other then 2: ");
-        int nonNegEvenNum = input.nextInt();
+        int nonNegEvenNum;
+        do {
+            System.out.print("Enter an even non-negative integer other than 2 (or 0 to exit): ");
+            nonNegEvenNum = input.nextInt();
+        } while (nonNegEvenNum != 0 && (nonNegEvenNum <= 2 || nonNegEvenNum % 2 != 0));
         return nonNegEvenNum;
     }
 
@@ -48,12 +70,12 @@ public class Goldbach {
         }
         for (int i = 2; i <= n / 2; i++) {
             if (isPrime(i) && isPrime(n - i)) {
-                System.out.println(n + " = " + i + " + " + (n - i));
+                System.out.println("Goldbach's conjecture is true for " + n + " = " + i + " + " + (n - i));
                 return;
             }
         }
         System.out.println("No prime pair found for " + n);
-    }
+    }   
 
     /**
      * Determine whether or not a number is prime
@@ -62,14 +84,15 @@ public class Goldbach {
      * @return true if n is prime, false otherwise
      */
     public static boolean isPrime(int n) {
-        if(n<=1){
+        if (n <= 1) {
             return false;
         }
-       for(int i=2;i<=n/2;i++){
-           if(n % i == 0)
-               return  false;
-       }
-       return true;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -79,12 +102,11 @@ public class Goldbach {
      * @return the next prime number after n
      */
     public static int getPrimeAfter(int n) {
-        if(n == 2) return 3;
+        if (n == 2) return 3;
         int nextPrimeNumber = n;
         do {
             nextPrimeNumber += 2;
-        } while(isPrime(nextPrimeNumber) == false);
+        } while (!isPrime(nextPrimeNumber));
         return nextPrimeNumber;
     }
-
 }
